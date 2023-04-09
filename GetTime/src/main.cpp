4dbc82ba3b2a2198c01383807164fbe28bba7035
稚子 <1973688584@qq.com>
2023-04-09 22:00:39 +0800

    first commit

--- GetTime/src/main.cpp
+++ GetTime/src/main.cpp
@@ -2,8 +2,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-const char *ssid = "leisure";
-const char *password = "shadow27";
+const char *ssid = "****";
+const char *password = "********";
 
 void get_time()
 {
